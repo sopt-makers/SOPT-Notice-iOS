aa6ff4f140ab814e95316f5a36c13160caa4bcd2
Subin Yang <cindy010670@gmail.com>
2022-10-06 11:45:11 +0900

    [Setting] #14 - Presentation 모듈 내부 폴더링

---
+++ SOPT-iOS/Projects/Presentation/Sources/SampleScene/VC/SampleVC.swift
@@ -0,0 +1,30 @@
+//
+//  SampleVC.swift
+//  Presentation
+//
+//  Created by 양수빈 on 2022/10/06.
+//  Copyright © 2022 SOPT-iOS. All rights reserved.
+//
+
+import UIKit
+
+class SampleVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SOPT-iOS/Projects/Presentation/Sources/SampleScene/ViewModel/SampleViewModel.swift
@@ -0,0 +1,44 @@
+//
+//  SampleViewModel.swift
+//  Presentation
+//
+//  Created by 양수빈 on 2022/10/06.
+//  Copyright © 2022 SOPT-iOS. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+//final class SampleViewModel: ViewModelType {
+//
+//    private let useCase: SampleUseCase
+//    private let cancelBag = Set<Cancellable>()
+//
+//    // MARK: - Inputs
+//    struct Input {
+//
+//    }
+//
+//    // MARK: - Outputs
+//    struct Output {
+//
+//    }
+//
+//    init(useCase: SampleUseCase) {
+//        self.useCase = useCase
+//    }
+//}
+//
+//extension SampleViewModel {
+//    func transform(from input: Input) -> Output {
+//        let output = Output()
+//        self.bindOutput(output: output)
+//        // input,output 상관관계 작성
+//
+//        return output
+//    }
+//
+//    private func bindOutput(output: Output) {
+//
+//    }
+//}
