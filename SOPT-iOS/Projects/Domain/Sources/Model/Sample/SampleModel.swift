a3cf9deb54dfd1229933b788d8bb7cf7c7ad1175
Subin Yang <cindy010670@gmail.com>
2022-10-06 11:05:29 +0900

    [Setting] #14 - Domain 모듈 내부 폴더링

---
+++ SOPT-iOS/Projects/Domain/Sources/Model/Sample/SampleModel.swift
@@ -0,0 +1,13 @@
+//
+//  SampleModel.swift
+//  Domain
+//
+//  Created by 양수빈 on 2022/10/06.
+//  Copyright © 2022 SOPT-iOS. All rights reserved.
+//
+
+import Foundation
+
+struct SampleModel {
+    
+}
---
+++ SOPT-iOS/Projects/Domain/Sources/RepositoryInterface/SampleRepositoryInterface.swift
@@ -0,0 +1,13 @@
+//
+//  SampleRepositoryInterface.swift
+//  Domain
+//
+//  Created by 양수빈 on 2022/10/06.
+//  Copyright © 2022 SOPT-iOS. All rights reserved.
+//
+
+import Combine
+
+protocol SampleRepositoryInterface {
+    
+}
