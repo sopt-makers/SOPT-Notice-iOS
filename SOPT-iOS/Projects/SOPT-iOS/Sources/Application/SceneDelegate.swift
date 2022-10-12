7b8fb884dc8eb20bcdcdf31b1e1ac1f4d802baca
L-j-h-c <ckrgkswnsgh@naver.com>
2022-10-12 16:15:25 +0900

    [Chore] #23 - 샘플파일 추가 삭제

--- SOPT-iOS/Projects/SOPT-iOS/Sources/Application/SceneDelegate.swift
+++ SOPT-iOS/Projects/SOPT-iOS/Sources/Application/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(frame: scene.coordinateSpace.bounds)
         window?.windowScene = scene
-        let rootViewController = ModuleFactory.shared.makeSampleVC()
+        let rootViewController = ModuleFactory.shared.makePostDetailVC()
         window?.rootViewController = rootViewController
         window?.makeKeyAndVisible()
     }
