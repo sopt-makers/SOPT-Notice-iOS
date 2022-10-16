a47be096221201568387293f4670b6470508c9de
L-j-h-c <ckrgkswnsgh@naver.com>
2022-10-17 03:52:16 +0900

    [Refactor] #31 - BaseService 간결하게 수정

--- SOPT-iOS/Projects/Modules/Network/Sources/Service/BaseService.swift
+++ SOPT-iOS/Projects/Modules/Network/Sources/Service/BaseService.swift
@@ -6,15 +6,96 @@
 //  Copyright © 2022 SOPT-iOS. All rights reserved.
 //
 
-import Alamofire
-import Moya
 import Combine
 import Foundation
 
-// FIXME: - 수정
+import Core
 
-open class BaseService {
-    public static let `default` = BaseService()
+import Alamofire
+import Moya
+
+open class BaseService<Target: TargetType> {
+    typealias API = Target
+    
     var cancelBag = Set<AnyCancellable>()
     private init() {}
+    public init() {}
+    
+    private lazy var provider: MoyaProvider<API> = {
+        let provider = MoyaProvider<API>(endpointClosure: endpointClosure, session: DefaultAlamofireManager.shared)
+        return provider
+    }()
+    
+    private let endpointClosure = { (target: API) -> Endpoint in
+        let url = target.baseURL.appendingPathComponent(target.path).absoluteString
+        var endpoint: Endpoint = Endpoint(url: url, sampleResponseClosure: {.networkResponse(200, target.sampleData)}, method: target.method, task: target.task, httpHeaderFields: target.headers)
+        return endpoint
+    }
+    func requestObject<T: Decodable>(_ target: API, completion: @escaping (Result<T?, Error>) -> Void) {
+        provider.request(target) { response in
+            switch response {
+            case .success(let value):
+                do {
+                    let decoder = JSONDecoder()
+                    let body = try decoder.decode(ResponseObject<T>.self, from: value.data)
+                    completion(.success(body.data))
+                } catch let error {
+                    completion(.failure(error))
+                }
+            case .failure(let error):
+                switch error {
+                case .underlying(let error, _):
+                    if error.asAFError?.isSessionTaskError ?? false {
+                        
+                    }
+                default: break
+                }
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    func requestArray<T: Decodable>(_ target: API, completion: @escaping (Result<[T], Error>) -> Void) {
+        provider.request(target) { response in
+            switch response {
+            case .success(let value):
+                do {
+                    let decoder = JSONDecoder()
+                    let body = try decoder.decode(ResponseObject<[T]>.self, from: value.data)
+                    completion(.success(body.data ?? []))
+                } catch let error {
+                    completion(.failure(error))
+                }
+            case .failure(let error):
+                switch error {
+                case .underlying(let error, _):
+                    if error.asAFError?.isSessionTaskError ?? false {
+                        
+                    }
+                default: break
+                }
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    func requestObjectWithNoResult(_ target: API, completion: @escaping (Result<Int?, Error>) -> Void) {
+        provider.request(target) { response in
+            switch response {
+            case .success(let value):
+                
+                completion(.success(value.statusCode))
+                
+            case .failure(let error):
+                switch error {
+                case .underlying(let error, _):
+                    if error.asAFError?.isSessionTaskError ?? false {
+                        
+                    }
+                default: break
+                }
+                completion(.failure(error))
+            }
+        }
+    }
 }
