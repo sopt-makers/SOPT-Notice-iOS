2024c678fb5b78c121cb4c04a18fff45f00b0421
L-j-h-c <ckrgkswnsgh@naver.com>
2022-10-16 23:17:50 +0900

    [Feat] #31 - AFManager 분리

---
+++ SOPT-iOS/Projects/Modules/Network/Sources/API/AuthAPI.swift
@@ -0,0 +1,9 @@
+//
+//  AuthAPI.swift
+//  Network
+//
+//  Created by Junho Lee on 2022/10/16.
+//  Copyright © 2022 SOPT-iOS. All rights reserved.
+//
+
+import Foundation
---
+++ SOPT-iOS/Projects/Modules/Network/Sources/API/NoticeAPI.swift
@@ -0,0 +1,9 @@
+//
+//  NoticeAPI.swift
+//  Network
+//
+//  Created by Junho Lee on 2022/10/16.
+//  Copyright © 2022 SOPT-iOS. All rights reserved.
+//
+
+import Foundation
