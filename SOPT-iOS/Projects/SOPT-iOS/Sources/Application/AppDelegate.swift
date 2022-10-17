e70c87f5bc5d37b11bcbee08d047992abaf5fdad
Subin Yang <cindy010670@gmail.com>
2022-10-17 15:36:23 +0900

    [Feat] #35 - APNS 기본 설정

--- SOPT-iOS/Projects/SOPT-iOS/Sources/Application/AppDelegate.swift
+++ SOPT-iOS/Projects/SOPT-iOS/Sources/Application/AppDelegate.swift
@@ -10,11 +10,32 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-    func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
+    func application( _ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // APNS 설정
+        UNUserNotificationCenter.current().delegate = self
+        
+        // APNS 권한 허용 알림
+        UNUserNotificationCenter.current()
+            .requestAuthorization(options: [.alert, .sound]) { granted, error in
+                // APNS 권한 성공
+                print("permission granted: \(granted)")
+        }
+        
+        // APNS 등록
+        application.registerForRemoteNotifications()
         return true
+    }
+    
+    /// APNS 등록 실패할 경우 호출되는 메서드
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Failed to register for notifications: \(error.localizedDescription)")
+    }
+    
+    /// APNS 등록 성공할 경우 호출되는 메서드
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let tokenPart = deviceToken.map { data in String(format: "%02.2hhx", data) }
+        let token = tokenPart.joined()
+        print("Device Token:", token)
     }
     
     // MARK: UISceneSession Lifecycle
@@ -31,4 +52,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didDiscardSceneSessions sceneSessions: Set<UISceneSession>
     ) {}
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    /// 앱이 실행 중(foreground)에 푸시을 받는 경우 처리하는 메소드
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.list, .banner, .sound])
+    }
 }
