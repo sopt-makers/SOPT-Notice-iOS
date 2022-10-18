97ca5d42f100ad90e3fa9a416bdd1a21cea2d3b4
L-j-h-c <ckrgkswnsgh@naver.com>
2022-10-18 22:46:41 +0900

    [Feat] #30 - 샘플 이미지 추가 및 ImageSlide present

--- SOPT-iOS/Projects/Presentation/Sources/PostDetailScene/VC/PostDetailVC.swift
+++ SOPT-iOS/Projects/Presentation/Sources/PostDetailScene/VC/PostDetailVC.swift
@@ -23,6 +23,7 @@ public class PostDetailVC: UIViewController {
     private var cancelBag = Set<AnyCancellable>()
     
     lazy var dataSource: UICollectionViewDiffableDataSource<Section, Int>! = nil
+    var sampleImages:[Image] = []
   
     // MARK: - UI Components
     
@@ -89,6 +90,27 @@ extension PostDetailVC {
         let output = self.viewModel.transform(from: input, cancelBag: self.cancelBag)
     }
 
+    private func presentImageSlide() {
+        ImageSlideShowViewController.presentByCustomTransitionFrom(self){ [weak self] controller in
+            controller.dismissOnPanGesture = false
+            controller.slides = self?.sampleImages
+            controller.enableZoom = true
+            controller.controllerDidDismiss = {
+                debugPrint("Controller Dismissed")
+
+                debugPrint("last index viewed: \(controller.currentIndex)")
+            }
+            controller.slideShowViewDidLoad = {
+                debugPrint("Did Load")
+            }
+            controller.slideShowViewWillAppear = { animated in
+                debugPrint("Will Appear Animated: \(animated)")
+            }
+            controller.slideShowViewDidAppear = { animated in
+                debugPrint("Did Appear Animated: \(animated)")
+            }
+        }
+    }
 }
 
 // MARK: - DiffableDataSource
@@ -137,4 +159,61 @@ extension PostDetailVC {
 
 extension PostDetailVC: UICollectionViewDelegate {
 
+}
+
+extension PostDetailVC: UIViewControllerTransitioningDelegate {
+    
+    public func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return ImageSlideShowViewController.presentLikePush
+    }
+    
+    public func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return ImageSlideShowViewController.dismissLikePush
+    }
+}
+
+extension PostDetailVC {
+    fileprivate func generateImages()
+    {
+        let scale:Int = Int(UIScreen.main.scale)
+        let height:Int = Int(view.frame.size.height) * scale
+        let width:Int = Int(view.frame.size.width) * scale
+        
+        sampleImages = [
+            Image(title: "Image 1", url: URL(string: "https://dummyimage.com/\(width)x\(height)/09a/fff.png&text=Image+1")!),
+            Image(title: "Image 2", url: URL(string: "https://dummyimage.com/\(600)x\(600)/09b/fff.png&text=Image+2")!),
+            Image(title: "Image 3", url: URL(string: "https://dummyimage.com/\(width)x\(height)/09c/fff.png&text=Image+3")!),
+            Image(title: "Image 4", url: URL(string: "https://dummyimage.com/\(600)x\(600)/09d/fff.png&text=Image+4")!),
+            Image(title: "Image 5", url: URL(string: "https://dummyimage.com/\(width)x\(height)/09e/fff.png&text=Image+5")!),
+            Image(title: "Image 6", url: URL(string: "https://dummyimage.com/\(width)x\(height)/09f/fff.png&text=Image+6")!),
+        ]
+    }
+}
+
+class Image: NSObject, ImageSlideShowProtocol
+{
+    private let url: URL
+    let title: String?
+    
+    init(title: String, url: URL) {
+        self.title = title
+        self.url = url
+    }
+    
+    func slideIdentifier() -> String {
+        return String(describing: url)
+    }
+    
+    func image(completion: @escaping (_ image: UIImage?, _ error: Error?) -> Void) {
+        let session = URLSession(configuration: URLSessionConfiguration.default)
+        session.dataTask(with: self.url) { data, response, error in
+            if let data = data,
+               error == nil {
+                let image = UIImage(data: data)
+                completion(image, nil)
+            } else {
+                completion(nil, error)
+            }
+        }.resume()
+    }
 }
