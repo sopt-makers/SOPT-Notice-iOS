a75337d937465de7b0c8d4baf8607304e93ee044
devxsby <devxsby@gmail.com>
2022-10-19 23:25:54 +0900

    [Feat] #32- SettingServiceType 추가

---
+++ SOPT-iOS/Projects/Modules/Network/Sources/Service/SettingService.swift
@@ -0,0 +1,16 @@
+//
+//  SettingService.swift
+//  Network
+//
+//  Created by devxsby on 2022/10/19.
+//  Copyright © 2022 SOPT-iOS. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+public protocol SettingServiceType {
+}
+
+extension BaseService: SettingServiceType {
+}
