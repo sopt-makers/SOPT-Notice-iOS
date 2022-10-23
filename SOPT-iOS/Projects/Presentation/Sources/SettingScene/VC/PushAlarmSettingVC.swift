71945179aa8681db25df2ecf467f12b41fced741
devxsby <devxsby@gmail.com>
2022-10-23 22:10:18 +0900

    [Chore] #24- 변수명 오타 수정

--- SOPT-iOS/Projects/Presentation/Sources/SettingScene/VC/PushAlarmSettingVC.swift
+++ SOPT-iOS/Projects/Presentation/Sources/SettingScene/VC/PushAlarmSettingVC.swift
@@ -31,7 +31,7 @@ public class PushAlarmSettingVC: UIViewController {
         return tableView
     }()
     
-    private let captionlabel = UILabel().then {
+    private let captionLabel = UILabel().then {
         $0.text = """
                   알림이 오지 않는다면 [기기 내 설정 > 알림]에서
                   알림 설정을 확인해주세요.
@@ -63,14 +63,14 @@ extension PushAlarmSettingVC {
     }
     
     private func setLayout() {
-        self.view.addSubviews(partListTableView, captionlabel)
+        self.view.addSubviews(partListTableView, captionLabel)
         
         partListTableView.snp.makeConstraints { make in
             make.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
             make.height.equalTo(56 * 7 + 1)
         }
         
-        captionlabel.snp.makeConstraints { make in
+        captionLabel.snp.makeConstraints { make in
             make.top.equalTo(partListTableView.snp.bottom).offset(8)
             make.leading.equalToSuperview().inset(16)
         }
