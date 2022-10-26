093869c7a62f968e0ff91fddbd6c134c94d91273
L-j-h-c <ckrgkswnsgh@naver.com>
2022-10-27 02:13:59 +0900

    [Feat] #58 - UIControl의 CustomPublisher 구현

---
+++ SOPT-iOS/Projects/Core/Sources/Extension/Combine+UIKit/CombineCompatible.swift
@@ -0,0 +1,11 @@
+//
+//  CombineCompatible.swift
+//  Core
+//
+//  Created by Junho Lee on 2022/10/27.
+//  Copyright © 2022 SOPT-iOS. All rights reserved.
+//
+
+import Combine
+
+public protocol CombineCompatible { }
