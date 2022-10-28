e20a686f7cd5cb4201fb0142470dd79dd08fabda
Sejin Lee <lsj8706@naver.com>
2022-10-29 00:59:32 +0900

    [Feat] #55 - PostListRepository에 Mock PostList를 가져오는 함수 추가

--- SOPT-iOS/Projects/Data/Sources/Repository/PostListRepository.swift
+++ SOPT-iOS/Projects/Data/Sources/Repository/PostListRepository.swift
@@ -26,6 +26,10 @@ extension PostListRepository: PostListRepositoryInterface {
     public func getSearchResult(str: String) -> AnyPublisher<[PostListModel]?, Error> {
         return makeMockSearchResultEntity()
     }
+    
+    public func getPostListResult(partName: String) -> AnyPublisher<[PostListModel]?, Error> {
+        return makeMokePostListResultEntity()
+    }
 }
 
 extension PostListRepository {
@@ -36,6 +40,19 @@ extension PostListRepository {
             PostSearchData(noticeID: 4, title: "3--th SOPT OT 공지", creator: "관리", createdAt: "10.1.1")
         ]
         let mockSearch = PostSearchEntity(notices: mockSearchData)
+        let model = mockSearch.toDomain()
+        return Just(model)
+                    .setFailureType(to: Error.self)
+                    .eraseToAnyPublisher()
+    }
+    
+    private func makeMokePostListResultEntity() -> AnyPublisher<[PostListModel]?, Error> {
+        let mockPostListData = [
+            PostListData(noticeID: 1, title: "31th SOPT OT 공지", creator: "관리", createdAt: "2022.5.13"),
+            PostListData(noticeID: 2, title: "31th SOPT 1차 행사 공지", creator: "관리", createdAt: "2022.6.10"),
+            PostListData(noticeID: 4, title: "31th SOPT 앱잼 공지", creator: "관리", createdAt: "2022.12.1")
+        ]
+        let mockSearch = PostListEntity(notices: mockPostListData)
         let model = mockSearch.toDomain()
         return Just(model)
                     .setFailureType(to: Error.self)
