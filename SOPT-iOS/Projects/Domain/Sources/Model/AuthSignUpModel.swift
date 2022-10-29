1b5d4cc98873e9da2da93c9732770716bcec42bd
devxsby <devxsby@gmail.com>
2022-10-29 17:45:42 +0900

    [Feat] #54- AuthSignUpModel 추가

--- SOPT-iOS/Projects/Domain/Sources/Model/AuthSignUpModel.swift
+++ SOPT-iOS/Projects/Domain/Sources/Model/AuthSignUpModel.swift
@@ -1,6 +1,6 @@
 //
 //  AuthSignUpModel.swift
-//  Presentation
+//  Domain
 //
 //  Created by devxsby on 2022/10/23.
 //  Copyright © 2022 SOPT-iOS. All rights reserved.
@@ -8,9 +8,10 @@
 
 import Foundation
 
-public struct AuthSignUpModel {
+public struct AuthSignUpModel: Hashable {
+    public let userId: Int
 
-    public init() {
-        
+    public init(userId: Int) {
+        self.userId = userId
     }
 }
